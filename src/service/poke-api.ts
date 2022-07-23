8a3fe13fa560538bd7c7620e188d62409cf87841
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-07-23 14:03:47 -0300

    fix: vercel deployment

--- src/service/poke-api.ts
+++ src/service/poke-api.ts
@@ -1,5 +1,5 @@
 export const fetchPokemon = async (pokemon: string | number) => {
-	const response = await fetch(`${import.meta.env.VITE_BASE_URL}${pokemon}`);
+	const response = await fetch(`${import.meta.env.VITE_POKE_URL}${pokemon}`);
 
 	if (response.status === 200) {
 		const data = await response.json();
